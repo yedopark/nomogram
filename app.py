74b46bc8fccf886014d94632e007eac8fb4f0638
yedopark <yedo1106@gmail.com>
2024-09-05 16:46:46 +0900

    Update app.py

--- app.py
+++ app.py
@@ -149,7 +149,7 @@ def calculate_impulse(df, volume, e_data_value):
 pressure_input = st.number_input("압력을 입력하세요 (MPa):", min_value=0.0, step=1.0)
 volume_input = st.number_input("부피를 입력하세요 (Liter):", min_value=0.0, step=1.0)
 
-if pressure_input in [20, 35, 70, 100]:
+if int(pressure_input) in [20, 35, 70, 100]:
     pressure_input += 0.1
 
 if st.button("계산 시작"):
