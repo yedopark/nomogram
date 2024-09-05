0a8404a77fe2e618c14edea56f3e0bdd5714826c
yedopark <yedo1106@gmail.com>
2024-09-05 17:05:18 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -149,17 +149,11 @@ def calculate_impulse(df, volume, e_data_value):
 pressure_input = st.number_input("압력을 입력하세요 (MPa):", min_value=0.0, step=1.0)
 volume_input = st.number_input("부피를 입력하세요 (Liter):", min_value=0.0, step=1.0)
 
-# 사용자에게 압력과 부피 입력 받기
-pressure_input = st.number_input("압력을 입력하세요 (MPa):", min_value=0.0, step=1.0)
-
 # 부동소수점 비교 시 허용 오차 범위를 설정
 tolerance = 0.001
-if any(abs(pressure_input - p) < tolerance for p in [20.00, 35.00, 70.00, 100.00]):
+if any(abs(pressure_input - p) < tolerance for p in [20, 35, 70, 100]):
     pressure_input += 0.1
     st.write(f"입력된 압력은 {pressure_input - 0.1} MPa이므로 {pressure_input} MPa로 간주합니다.")
-else:
-    st.write("압력이 20, 35, 70, 100 중 하나가 아닙니다.")
-
 
 if st.button("계산 시작"):
     # 엑셀 파일 읽기
@@ -231,7 +225,7 @@ if st.button("계산 시작"):
     st.write(f"D_data 길이: {len(D_data)}")
     st.write(f"E_data 길이: {len(E_data)}")
     st.write(f"Impulse_data 길이: {len(Impulse_data)}")
-
+    
     # 배열들의 길이를 동일하게 맞춤 (Overpressure 관련 데이터)
     min_length_overpressure = min(len(A_data), len(B_data_interpolated), len(overpressure_values))
     A_data = A_data.iloc[:min_length_overpressure]
