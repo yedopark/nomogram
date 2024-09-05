6b121463dad008c010379862312914eb89283194
yedopark <yedo1106@gmail.com>
2024-09-05 13:58:50 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -209,12 +209,25 @@ if st.button("계산 시작"):
     status_text.text("Finalizing data...")
     
     st.write(f"A_data 길이: {len(A_data)}")
+    st.write(f"A_data : {A_data}")
+    
     st.write(f"B_data 길이: {len(B_data_interpolated)}")
+    st.write(f"A_data : {B_data_interpolated}")
+    
     st.write(f"Overpressure 길이: {len(overpressure_values)}")
+    st.write(f"A_data : {overpressure_values}")
+    
     st.write(f"C_data 길이: {len(C_data)}")
+    st.write(f"A_data : {C_data}")
+    
     st.write(f"D_data 길이: {len(D_data)}")
+    st.write(f"A_data : {D_data}")
+    
     st.write(f"E_data 길이: {len(E_data)}")
+    st.write(f"A_data : {E_data}")
+    
     st.write(f"Impulse_data 길이: {len(Impulse_data)}")
+    st.write(f"A_data 길이: {Impulse_data}")
     
     # 배열들의 길이를 동일하게 맞춤
     min_length = min(len(A_data), len(B_data_interpolated), len(overpressure_values), len(C_data), len(D_data), len(E_data), len(Impulse_data))
